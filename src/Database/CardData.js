f7a20c1e67bc97918ea0052fb3cfcb4c0efeafdf
nthplt <114916781+nthplt@users.noreply.github.com>
2023-04-01 13:41:56 +0200

    add menu

--- src/Database/CardData.js
+++ src/Database/CardData.js
@@ -2,53 +2,69 @@ const cardList = [
     {
         title: "Poisson Pané",
         img: "./assets/images/monster.png",
-        type: "attack",
-        manaCost : 3,
-        damage: 4,
+        attacks: {
+            type: "attack",
+            manaCost : 3,
+            damage: 4,
+        }
     },
     {
         title: "Bain d'eau douce",
         img: "./assets/images/monster.png",
-        manaCost : 4,
-        type: "support",
-        heal: 6,
+        attacks: {
+            manaCost : 4,
+            type: "support",
+            heal: 6,
+        }
     },
     {
         title: "Huile",
         img: "./assets/images/monster.png",
-        manaCost : 2,
-        type: "support",
-        cleaning: true,
+        attacks: {
+            manaCost : 2,
+            type: "support",
+            cleaning: true,
+        }
     },
     {
         title: "Bouclier Divin",
         img: "./assets/images/monster.png",
-        manaCost : 9,
-        type: "support",
-        immunity: true,
+        attacks: {
+            manaCost : 9,
+            type: "support",
+            immunity: true,
+        }
     },
     {
         title: "Coup de crâne chauve",
         img: "./assets/images/monster.png",
-        manaCost : 7,
-        type: "attack",
-        damage: 3,
-        bleeding: 3,
+        attacks: {
+            manaCost : 7,
+            type: "attack",
+            damage: 3,
+            bleeding: 3,
+        }
     },
     {
         title: "Oui",
         img: "./assets/images/monster.png",
-        manaCost : 4,
-        type: "attack",
-        damage: 0,
-        stun: 1,
+        attacks: {
+            manaCost : 4,
+            type: "attack",
+            damage: 0,
+            stun: 1,
+        }
     },
     {
         title: "Roulette russe",
         img: "./assets/images/monster.png",
-        manaCost : 8,
-        type: "attack",
-        damage: 15,
-        random: true,
+        attacks: {
+            manaCost : 8,
+            type: "attack",
+            damage: 15,
+            random: true,
+        }
     },
 ];
+
+export default cardList
