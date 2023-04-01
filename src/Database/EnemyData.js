7919a89ee347898070d591b782e82b721dc81a2e
nthplt <114916781+nthplt@users.noreply.github.com>
2023-04-01 11:16:56 +0200

    add card and enemys

---
+++ src/Database/EnemyData.js
@@ -0,0 +1,22 @@
+const enemyList = [
+    {
+        name: "Adam",
+        img: "./assets/images/monster.png",
+        description: 'Le troll des océans',
+        attacks: [
+            {
+                type: 'attack',
+                damage: 6,
+            },
+            {
+                type: 'support',
+                heal: 5,
+            },
+            {
+                type: 'attack',
+                bleeding: 3,
+                damage: 5,
+            }
+        ],
+    },
+]
